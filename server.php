019b802f4e61568d52336c5466389322841f3ca5
张贯宇 <garychangcn@hotmail.com>
2016-05-15 21:56:12 +0800

    jsonp支持

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
-//$a=$_GET['name1'];
-//$b=$_GET['name2'];
-$a=$_POST['key'];
-$b=$_POST['key2'];
+$a=$_GET['key'];
+$b=$_GET['key2'];
+// $a=$_POST['key'];
+// $b=$_POST['key2'];
 $arr=array("key"=>$a,"key2"=>$b);
 echo json_encode($arr)
 ?>
